df60808c4b7b111d0f9e609a823be83e14a64023
ferz55 <r.abishev55@yandex.ru>
2021-03-28 18:41:49 +0600

    Create Лаба4.cpp

---
+++ Лаба4.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include<math.h>
+#include<stdlib.h>
+
+using namespace std;
+
+int fibonacci_sequence(int number);
+
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+
+	cout << "Данная программа вычисляет числа из последовательности Фибоначчи (0,1,1,2,3,5,8,13,21...)" << endl;
+
+	int number;
+	cout << "Введите номер числа из последовательсти: " << endl;
+	cin >> number;
+
+	for (int i = 0; i <= number; i++) {
+		cout << fibonacci_sequence(i) << " ";
+	}
+}
+
+int fibonacci_sequence(int number)
+{
+	if ((number == 0) || (number == 1))
+	{
+		return number;
+	}
+	else
+	{
+		return fibonacci_sequence(number - 1) + fibonacci_sequence(number - 2);
+	}
+}
