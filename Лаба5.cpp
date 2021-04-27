dab67e969efb1724a36f9f69e325c1881fa5502d
ferz55 <r.abishev55@yandex.ru>
2021-04-27 20:56:50 +0600

    Create Лаба5.cpp

---
+++ Лаба5.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+string coder(string text)
+{
+    for (int i = 0; i < text.length(); i++)
+    {
+        char letter = text[i];
+
+        if ((letter >= 'a' && letter <= 'm') || (letter >= 'A' && letter <= 'M'))
+        {
+            text[i] += 1;
+        }
+        else if ((letter >= 'n' && letter <= 'z') || (letter >= 'N' && letter <= 'Z'))
+        {
+            text[i] -= 1;
+        }
+    }
+
+    return text;
+}
+
+int main()
+{
+    string text = "Hello, world!";
+
+    text = coder(text);
+    cout << "Encoded text: " << text << endl;
+
+
+
+    return 0;
+}
