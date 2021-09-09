cd4c304abe54e4b70cab638e115046c82ebf726c
ferz55 <r.abishev55@yandex.ru>
2021-09-09 19:25:51 +0600

    Create MenuItemHelloWorld.cs

---
+++ c#/1 лаба/MenuItemHelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_лаба.MainItems
+{
+    public class MenuItemHelloWorld : MenuItemCore
+    {
+        public override string Title { get { return "Hello World!"; } }
+
+        public override void Execute()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
