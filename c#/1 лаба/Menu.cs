71fde0796336e1ce9972df9cae807b622a5d89e1
ferz55 <r.abishev55@yandex.ru>
2021-09-09 19:25:44 +0600

    Create Menu.cs

---
+++ c#/1 лаба/Menu.cs
@@ -0,0 +1,47 @@
+﻿using _1_лаба.MainItems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_лаба
+{
+    public class Menu
+    {
+       private static List<MenuItemCore> MenuItems = new List<MenuItemCore>();
+
+        public static void ClearItems()
+        {
+            Menu.MenuItems.Clear();
+        }
+        public static  void AddItem(MenuItemCore menuItem)
+        {
+            Menu.MenuItems.Add(menuItem);
+        }
+
+        public static void Execute()
+        {
+            ShowMenu();
+            int iMenu = IOUtils.SafeReadInteger(null);
+            if (iMenu >= 0 && iMenu < Menu.MenuItems.Count)
+            {
+                Menu.MenuItems.ToArray()[iMenu].Execute();
+            }
+            else
+            {
+                Console.WriteLine("Menu item not found...");
+            }
+        }
+        private static void ShowMenu()
+        {
+            Console.WriteLine("=== MENU ===");
+
+            int iMenuItem = 0;
+            foreach (MenuItemCore menuItem in Menu.MenuItems)
+            {
+                Console.WriteLine("{0}: {1} ", iMenuItem++, menuItem.Title);
+            }
+        }
+    }
+}
