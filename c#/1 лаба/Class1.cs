25f9febecc6713e154fabc047ae563aaca98a494
ferz55 <r.abishev55@yandex.ru>
2021-09-09 19:25:39 +0600

    Create Class1.cs

---
+++ c#/1 лаба/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_лаба.MainItems
+{
+    public class Class1 : MenuItemCore
+    {
+        public override string Title { get { return "Calculate expression: Y^2+3*sqrt(X)+Z"; } }
+
+        public override void Execute()
+        {
+            bool number;
+            double X,Y,Z, result, sqrt;
+            do
+            {
+                Console.WriteLine("Enter X: ");
+                number = Double.TryParse(Console.ReadLine(), out X);
+                if (!(number))
+                    Console.WriteLine("Error.....");
+            } while (!number);
+            do
+            {
+                Console.WriteLine("Enter Y: ");
+                number = Double.TryParse(Console.ReadLine(), out Y);
+                if (!(number))
+                    Console.WriteLine("Error.....");
+            } while (!number);
+            do
+            {
+                Console.WriteLine("Enter Z: ");
+                number = Double.TryParse(Console.ReadLine(), out Z);
+                if (!(number))
+                    Console.WriteLine("Error.....");
+            } while (!number);
+
+            sqrt = Math.Sqrt(X);
+
+            result = Y * Y + 3 * sqrt + Z;
+            Console.WriteLine("Calculation result: {0}", result);
+        }
+    }
+}
