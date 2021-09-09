a969125dc87f7e38e76766dc3d717518dc474efd
ferz55 <r.abishev55@yandex.ru>
2021-09-09 19:25:53 +0600

    Create Program.cs

---
+++ c#/1 лаба/Program.cs
@@ -0,0 +1,24 @@
+﻿using _1_лаба.MainItems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_лаба
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.ClearItems();
+            Menu.AddItem(new MenuItemExit());
+            Menu.AddItem(new MenuItemHelloWorld());
+            Menu.AddItem(new Class1());
+            while (true)
+            {
+                Menu.Execute();
+            }
+        }
+    }
+}
