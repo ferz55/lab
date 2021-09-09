d5f954f084835eb15554d136bc7342da45ab2bf8
ferz55 <r.abishev55@yandex.ru>
2021-09-09 19:25:49 +0600

    Create MenuItemExit.cs

---
+++ c#/1 лаба/MenuItemExit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_лаба.MainItems
+{
+    public class MenuItemExit : MenuItemCore
+    {
+        public override string Title { get { return "Exit"; } }
+
+        public override void Execute()
+        {
+            Console.WriteLine("Exit...");
+            Environment.Exit(0);
+        }
+    }
+}
