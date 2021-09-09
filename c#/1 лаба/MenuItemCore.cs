e57635391c26267f27410d36a87dea00fd750700
ferz55 <r.abishev55@yandex.ru>
2021-09-09 19:25:47 +0600

    Create MenuItemCore.cs

---
+++ c#/1 лаба/MenuItemCore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_лаба.MainItems
+{
+    public abstract class MenuItemCore
+    {
+        public abstract string Title { get; }
+        public abstract void Execute();
+    }
+}
